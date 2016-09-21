0e537f7a8cae96c02be07a03288bde99618f00a3
Nicholas Michel <nickmichel100@gmail.com>
2016-09-21 17:35:40 -0400

    basic devise functional

--- GoogleTours/db/migrate/20160921213205_create_tours.rb
+++ GoogleTours/db/migrate/20160921213205_create_tours.rb
@@ -1,7 +1,7 @@
 class CreateTours < ActiveRecord::Migration[5.0]
   def change
     create_table :tours do |t|
-      t.string :images
+      t.string :image
       t.references :user, foreign_key: true
 
       t.timestamps
