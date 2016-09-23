d3adca81d91fb397ace8e17bd5ce38c91199cfcf
Nicholas Michel <nickmichel100@gmail.com>
2016-09-23 13:35:51 -0400

    working save to database, blurb creation

--- GoogleTours/app/controllers/tours_controller.rb
+++ GoogleTours/app/controllers/tours_controller.rb
@@ -1,4 +1,10 @@
 class ToursController < ApplicationController
   def index
+    @tours = Tour.all
+  end
+
+  def create
+    data = params[:tour]
+    Tour.create(image: data, user_id: current_user.id)
   end
 end
